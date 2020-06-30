0a2df1443290748630a6c9c5723193b4da87d3af
Mitchell Foote <mitchellfoote93@gmail.com>
2020-06-30 11:39:56 -0600

    cleaned up some files

--- packages/terminal-puzzle/stories/testfile.story.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { storiesOf } from "@storybook/react"
-import TestClass from '../src/starting-folder'
-storiesOf("Test class", module).add('test storybook', () => {
-    return (<>
-        <TestClass />
-    </>)
-})
